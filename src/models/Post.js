26b5b531e184db09c935e87c14bce16101f6e3a0
melanie <mjharv@hotmail.com>
2024-12-06 11:33:02 -0700

    got model

---
+++ src/models/Post.js
@@ -0,0 +1,11 @@
+export class Post {
+
+    constructor(data) {
+        this.body = data.body
+        this.imgUrl = data.imgUrl
+        this.creatorId = data.creatorId
+        this.likeIds = data.likeIds
+        this.id = data.id
+    }
+
+}
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { postsService } from '@/services/PostsService';
+import { logger } from '@/utils/Logger';
+import Pop from '@/utils/Pop';
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+  getPosts()
+})
+
+async function getPosts() {
+  try {
+    await postsService.getPosts()
+  }
+  catch (error) {
+    Pop.meow(error);
+    logger.error('[Getting posts 💬]', error)
+  }
+
+}
+
+</script>
+
+<template>
+  <div class="">
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
